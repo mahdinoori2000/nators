264eecf3853704141500e99ae0ab09d540fd9179
Mahdi Noori <mahdinoori2389@gmail.com>
2024-04-04 01:19:59 +0430

    Add git method for the tours

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello, this comes from a server', app: 'Natours' });
-});
-app.post('/', (req, res) => {
-  res.send('You can send request to this URL');
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello, this comes from a server', app: 'Natours' });
+// });
+// app.post('/', (req, res) => {
+//   res.send('You can send request to this URL');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({ message: 'success', result: tours.length, tours });
 });
 
 const port = '3000';
