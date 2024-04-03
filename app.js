9f84c25131a20b19c6c8cf9fc5a23dd98af27239
Mahdi Noori <mahdinoori2389@gmail.com>
2024-04-04 01:49:07 +0430

    Add post state for API

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
 //     .json({ message: 'Hello, this comes from a server', app: 'Natours' });
 // });
-// app.post('/', (req, res) => {
-//   res.send('You can send request to this URL');
-// });
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tours: newTour } });
+    }
+  );
+});
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({ message: 'success', result: tours.length, tours });
