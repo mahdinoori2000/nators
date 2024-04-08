e9ee35d06f47100d37f61192d2d94d36a3841900
Mahdi Noori <mahdinoori2389@gmail.com>
2024-04-09 01:58:05 +0430

    Serve static files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from Middleware');
