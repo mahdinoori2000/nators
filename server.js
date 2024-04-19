65685f71e033ed50d52f35bee2add34c7501a7dd
Mahdi Noori <mahdinoori2389@gmail.com>
2024-04-19 17:15:10 +0430

    Connect app with database.

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('Database connected succesfully!');
+  });
 
 const port = '3000';
 app.listen(port, () => {
