bd1fbc283d592fa230a5315d9aed7daeff705558
Mahdi Noori <mahdinoori2389@gmail.com>
2024-04-19 21:32:34 +0430

    Add post tour functionality.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,58 +1,45 @@
 const fs = require('fs');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('../models/tourModel');
 
-exports.checkId = (req, res, next, val) => {
-  if (val > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-  next();
-};
+// exports.checkId = (req, res, next, val) => {
+//   // if (val > tours.length) {
+//   //   return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+//   // }
+//   // next();
+// };
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.difficulty || !req.body.price) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid input of difficulty or price',
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      message: 'success',
+      data: {
+        tour: newTour,
+      },
     });
+  } catch (err) {
+    res.status(400).json({ status: 'fail', message: 'Invalid tour send' });
   }
-  next();
-};
-
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({ status: 'success', data: { tours: newTour } });
-    }
-  );
 };
 
 exports.getAllTours = (req, res) => {
-  res.status(200).json({ message: 'success', result: tours.length, tours });
+  // res.status(200).json({ message: 'success', result: tours.length, tours });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find((el) => el.id == id);
-
-  if (!tour) {
-    res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-  res.status(200).json({
-    stutas: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id == id);
+  // if (!tour) {
+  //   res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  // }
+  // res.status(200).json({
+  //   stutas: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.updateTour = (req, res) => {
