ce7ae6e28684a784e4f8bc684c92377e54fba3cd
Mahdi Noori <mahdinoori2389@gmail.com>
2024-05-04 09:06:14 +0430

    Add fields to the tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,8 +25,6 @@ exports.getAllTours = async (req, res) => {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    let query = Tour.find(JSON.parse(queryStr));
-
     // const query = Tour.find()
     // .where('duration')
     // .equals(5)
@@ -37,12 +35,21 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
+    let query = Tour.find(JSON.parse(queryStr));
     // 2) SORTING
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    // 3) FIELDS
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     // EXECUTE QUERY
